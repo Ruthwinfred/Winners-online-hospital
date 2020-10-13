ba82d4312fad53d51391a81744c3b4a5b0167dd8
Ruthwinfred <53862733+Ruthwinfred@users.noreply.github.com>
2020-10-13 10:28:43 +0300

    add the modified changes

---
+++ APPI/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Login</title>
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<?php
+require('db.php');
+session_start();
+// If form submitted, insert values into the database.
+if (isset($_POST['username'])){
+        // removes backslashes
+ $username = stripslashes($_REQUEST['username']);
+        //escapes special characters in a string
+ $username = mysqli_real_escape_string($con,$username);
+ $password = stripslashes($_REQUEST['password']);
+ $password = mysqli_real_escape_string($con,$password);
+ //Checking is user existing in the database or not
+        $query = "SELECT * FROM `users` WHERE username='$username'
+and password='".md5($password)."'";
+ $result = mysqli_query($con,$query) or die(mysql_error());
+ $rows = mysqli_num_rows($result);
+        if($rows==1){
+     $_SESSION['username'] = $username;
+            // Redirect user to index.php
+     header("Location: Grid.html");
+         }else{
+ echo "<div class='form'>
+<h3>Username/password is incorrect.Click here to <a href='login.php'>try again</a> or <a href='Registration.php'>register</a></h3></div>";
+ }
+    }else{
+?>
+<div class="form">
+<h1>Log In</h1>
+<form action="" method="post" name="login">
+<input type="text" name="username" placeholder="Username" required />
+<input type="password" name="password" placeholder="Password" required />
+<input name="submit" type="submit" value="Login" />
+</form>
+<h3>Not registered yet? <a id='a1' href='registration.php'>Register Here</a></h3>
+</div>
+<?php } ?>
+</body>
+</html>
---
+++ APPI/ses.php
@@ -0,0 +1,7 @@
+<?php
+    session_start();
+    if(!isset($_SESSION["UserName"])) {
+        header("Location: login.php");
+        exit();
+    }
+?>

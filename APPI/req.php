193b0f28a06c8970e05a898400dff8fef6954594
Ruthwinfred <53862733+Ruthwinfred@users.noreply.github.com>
2020-10-16 16:39:09 +0300

    Add all modified files

--- APPI/req.php
+++ APPI/req.php
@@ -2,28 +2,41 @@
 <html>
 <head>
 	<title>App1</title>
+	<link rel="stylesheet" type="text/css" href="req.css">
 </head>
 <body>
+	<div class="col">
 <form method="post">
-    <input type="text" name="PName" placeholder="Patient name" title="The official names of the patient; the one to be treated"><br><br>
-    <input type="text" name="Age" placeholder="Age" title="How old is the patient to be treated"><br><br>
-    Gender:<input type="radio" name="gender" value="male">Male&nbsp;&nbsp;&nbsp;
-    <input type="radio" name="gender" value="female">Female&nbsp;&nbsp;&nbsp;
-    <input type="radio" name="gender" value="other">Other<br><br>
-    <textarea rows="4" cols="50" title="Why do you want to see the doctor?" placeholder="state your problem here"></textarea><br><br>
+	<fieldset id="leg">
+		<legend>Submit your request here</legend>
+    <input type="text" name="PName" placeholder="Patient name" title="The official names of the patient; the one to be treated" required><br><br>
+    <input type="text" name="Age" placeholder="Age" title="How old is the patient to be treated" required><br><br>
+    Gender:<input type="radio" name="Gender" value="male" required>Male&nbsp;&nbsp;&nbsp;
+    <input type="radio" name="Gender" value="female" required>Female&nbsp;&nbsp;&nbsp;
+    <input type="radio" name="Gender" value="other" required>Other<br><br>
+    <textarea rows="4" cols="50" title="Why do you want to see the doctor?" placeholder="state your problem here" name="Problem" required></textarea><br><br>
     <hr>
-    <input type="text" name="response" placeholder="Feedback contact" title="How should the hospital get back to you after reviewing your problem?"><br><br>
-    Appointment via:<input type="radio" name="comm" value="video">Video&nbsp;&nbsp;&nbsp;
-    <input type="radio" name="comm" value="Audio">Audio&nbsp;&nbsp;&nbsp;
-    <input type="radio" name="comm" value="text">Text<br><br>
-    <input type="submit" name="register" value="Register">
+    <input type="text" name="Fdback" placeholder="Feedback contact" title="How should the hospital get back to you after reviewing your problem?" required><br><br>
+    Appointment via:<select name="Appointment">
+	<option required>Video call</option>
+	<option required>Audio call</option>
+	<option required>Text chat</option>
+    </select><br><br>
+	<input type="datetime-local" name="Date" placeholder="Date and time" title="The exact date and time the transaction was made" required><br><br>
+	<input type="submit" name="sub" value="Submit">
+</fieldset>
 </form>
-
+</div>
 <?php
-	if (isset($_POST['register'])) {
-		$usr = $_POST['username'];
- $em = $_POST['email'];
- $pwd = $_POST['password'];
+	if (isset($_POST['sub'])) {
+		$pname = $_POST['PName'];
+		$age = $_POST['Age'];
+		$gnder = $_POST['Gender'];
+		$prob = $_POST['Problem'];
+		$fbk = $_POST['Fdback'];
+		$app = $_POST['Appointment'];
+		$dte = $_POST['Date'];
+		
 
  // echo $usr;
  // echo $em;
@@ -33,7 +46,7 @@
 $servername = 'localhost';
 $dbusername = "root";
 $dbpassword = "";
-$dbname = "enock";
+$dbname = "register";
 
 //connecting to the database
 $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
@@ -44,13 +57,14 @@ if ($conn->connect_error) {
 }
 
 //insert values into db
-	$sql = "INSERT INTO details(username,email, password) VALUES('$usr','$em','$pwd')";
+	$sql = "INSERT INTO patients(PName,Age,Gender,Problem,Fdback,Appointment,Date) VALUES('$pname','$age','$gnder','$prob','$fbk','$app','$dte')";
 
 	//execute the query
 	if ($conn->query($sql) === TRUE) {
-		header('Location: index.php');
+		echo "<left>Dear  ".$pname. ",  your request has been submitted successfully. You can now <a href='logout.php'>end the session</a> or <a href='Grid.html'>go back</a> 
+		as you await for your appointment notice.</left>";
 	}else{
-		echo "Failled to register";
+		echo "Failled to register. Try again or <a href='Grid.html'>go back.</a>";
 	}
 
 	$conn->close();
